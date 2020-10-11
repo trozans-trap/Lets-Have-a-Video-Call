7b99e8aa8a7344792b2ca07e74e993dfaabaca59
trozans-trap <chawlavikalp2000@gmail.com>
2020-10-11 12:20:13 +0530

    video working and minor css

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const videoGrid = document.getElementById('video-grid');
+console.log(videoGrid);
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
@@ -8,11 +10,12 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStram = stream;
     addVideoStream(myVideo, stream);
-});
+})
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
         video.play();
-    })
+    });
+    videoGrid.append(video);
 }
