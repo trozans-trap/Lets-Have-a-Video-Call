30ae073a3f6527f03c1148c4a798788cbb052e86
trozans-trap <chawlavikalp2000@gmail.com>
2020-10-11 12:04:37 +0530

    room + video stram (in js)

---
+++ public/script.js
@@ -0,0 +1,18 @@
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+let myVideoStram;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(stream => {
+    myVideoStram = stream;
+    addVideoStream(myVideo, stream);
+});
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+}
